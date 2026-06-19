f0778925d104b470ec5a92dc0d12bf119aac0b18
mdawson <m.dawson054@mybvc.ca>
2026-06-19 15:57:11 -0600

    diagetic washing mashine

---
+++ SpinToWin/Assets/_Project/Code/UI/TitleScreen.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project.Code.UI
+{
+    public class TitleScreen : MonoBehaviour
+    {
+        
+        [SerializeField] private Image washingMachine;
+        [SerializeField] private float timeToWait;
+        [SerializeField] private float shakeSpeed;
+        [SerializeField] private RectTransform shakepointA;
+        [SerializeField] private RectTransform shakepointB;
+        [SerializeField] private bool shaking;
+        
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            shakepointA.position = washingMachine.rectTransform.position;
+            
+            StartCoroutine(ThrowItBack());
+        }
+        
+        
+        private IEnumerator ThrowItBack()
+        {
+            while (shaking)
+            {
+                yield return new WaitForSeconds(timeToWait);
+                while (Vector2.Distance(washingMachine.rectTransform.position, shakepointB.position) > 0.01f)
+                {
+                    washingMachine.rectTransform.position =  Vector2.MoveTowards(washingMachine.rectTransform.position, shakepointB.position, shakeSpeed * Time.deltaTime);
+                    yield return null;
+                    Debug.Log("moving");
+                }
+            
+                yield return new WaitForSeconds(timeToWait);
+                while (Vector2.Distance(washingMachine.rectTransform.position, shakepointA.position) > 0.01f)
+                {
+                    washingMachine.rectTransform.position =  Vector2.MoveTowards(washingMachine.rectTransform.position, shakepointA.position, shakeSpeed * Time.deltaTime);
+                    yield return null;
+                }
+            }
+
+            yield return null;
+        }
+    }
+}
